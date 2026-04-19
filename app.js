9c5597cb12fff225f5924611757c8ec62ac38939
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-20 04:47:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,19 @@
   let currentinput = "0";
   let errormode = false;
   let justEvaluated = false;
-  
+
+  function opToSymbol(op) {
+    switch (op) {
+      case "*":
+        return "\u00d7";
+      case "/":
+        return "\u00f7";
+      case "+":
+        return "+";
+      case "-":
+        return "-";
+      defualt:
+        return op;
+    }
+  }
 }
